6092e27fa6f7bcc1329bae70dfed1f3c19811153
yemretikbas-lgtm <yemre.tikbas@gmail.com>
2026-09-24 18:57:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'afet-saha-tespit-v0.12.5.1-outputfix-20260921';
+const CACHE_NAME = 'afet-saha-tespit-v0.12.7-json-birlestir-20260924';
 const APP_SHELL = [
   './',
   './index.html',
